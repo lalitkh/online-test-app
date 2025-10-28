a3a36154f92bd4f53532ad0f5abfff64bd96f568
Lalit Khandelwal <Lalit_Khandelwal@dell.com>
2025-10-28 12:28:25 +0530

    online test app

--- src/App.tsx
+++ src/App.tsx
@@ -29,71 +29,71 @@ interface Subject {
 }
 
 const subjects: Subject[] = [
-  {
-    id: 'dcl_iq_gk1',
-    name: 'DCL IQ GK-1',
-    questionsFile: 'dcl_iq_gk1.json'
-  },
-  {
-    id: 'dcl_iq_gk2',
-    name: 'DCL IQ GK-2',
-    questionsFile: 'dcl_iq_gk2.json'
-  },
-  {
-    id: 'dcl_iq_gk3',
-    name: 'DCL IQ GK-3',
-    questionsFile: 'dcl_iq_gk3.json'
-  },
-  {
-    id: 'dcl_iq_gk4',
-    name: 'DCL IQ GK-4',
-    questionsFile: 'dcl_iq_gk4.json'
-  },
+  // {
+  //   id: 'dcl_iq_gk1',
+  //   name: 'DCL IQ GK-1',
+  //   questionsFile: 'dcl_iq_gk1.json'
+  // },
+  // {
+  //   id: 'dcl_iq_gk2',
+  //   name: 'DCL IQ GK-2',
+  //   questionsFile: 'dcl_iq_gk2.json'
+  // },
+  // {
+  //   id: 'dcl_iq_gk3',
+  //   name: 'DCL IQ GK-3',
+  //   questionsFile: 'dcl_iq_gk3.json'
+  // },
+  // {
+  //   id: 'dcl_iq_gk4',
+  //   name: 'DCL IQ GK-4',
+  //   questionsFile: 'dcl_iq_gk4.json'
+  // },
   // {
   //   id: 'dcl_iq_math',
   //   name: 'DCL IQ Math',
   //   questionsFile: 'dcl_iq_math.json'
   // },
-  {
-    id: 'dcl_iq_mock1',
-    name: 'DCL IQ Mock Test 1',
-    questionsFile: 'dcl_iq_mock1.json'
-  },
-  {
-    id: 'dcl_iq_mock2',
-    name: 'DCL IQ Mock Test 2',
-    questionsFile: 'dcl_iq_mock2.json'
-  },
-  {
-    id: 'dcl_iq_mock3',
-    name: 'DCL IQ Mock Test 3',
-    questionsFile: 'dcl_iq_mock3.json'
-  },
-  {
-    id: 'dcl_iq_mock4',
-    name: 'DCL IQ Mock Test 4',
-    questionsFile: 'dcl_iq_mock4.json'
-  },
-  {
-    id: 'dcl_model_test',
-    name: 'DCL IQ Model Test',
-    questionsFile: 'dcl_model_test.json'
-  },
-  {
-    id: 'kisa_set1',
-    name: 'KISA Set-1',
-    questionsFile: 'kisa_set1.json'
-  },
-  {
-    id: 'kisa_set2',
-    name: 'KISA Set-2',
-    questionsFile: 'kisa_set2.json'
-  },
-  {
-    id: 'kisa_set3',
-    name: 'KISA Set-3',
-    questionsFile: 'kisa_set3.json'
-  },
+  // {
+  //   id: 'dcl_iq_mock1',
+  //   name: 'DCL IQ Mock Test 1',
+  //   questionsFile: 'dcl_iq_mock1.json'
+  // },
+  // {
+  //   id: 'dcl_iq_mock2',
+  //   name: 'DCL IQ Mock Test 2',
+  //   questionsFile: 'dcl_iq_mock2.json'
+  // },
+  // {
+  //   id: 'dcl_iq_mock3',
+  //   name: 'DCL IQ Mock Test 3',
+  //   questionsFile: 'dcl_iq_mock3.json'
+  // },
+  // {
+  //   id: 'dcl_iq_mock4',
+  //   name: 'DCL IQ Mock Test 4',
+  //   questionsFile: 'dcl_iq_mock4.json'
+  // },
+  // {
+  //   id: 'dcl_model_test',
+  //   name: 'DCL IQ Model Test',
+  //   questionsFile: 'dcl_model_test.json'
+  // },
+  // {
+  //   id: 'kisa_set1',
+  //   name: 'KISA Set-1',
+  //   questionsFile: 'kisa_set1.json'
+  // },
+  // {
+  //   id: 'kisa_set2',
+  //   name: 'KISA Set-2',
+  //   questionsFile: 'kisa_set2.json'
+  // },
+  // {
+  //   id: 'kisa_set3',
+  //   name: 'KISA Set-3',
+  //   questionsFile: 'kisa_set3.json'
+  // },
   {
     id: 'india_quiz_set1',
     name: 'India Quiz Set 1',
